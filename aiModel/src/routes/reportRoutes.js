b511b9328e0db03f439fc6338a14081cb21c6bd4
mohamed1fares <110127407+mohamed1fares@users.noreply.github.com>
2026-03-11 23:39:00 +0200

    fixed yassa bha

--- aiModel/src/routes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/:scanId', reportController.generateAndDownloadPDF);
-
-module.exports = router;
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+// server.js
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.config');
@@ -6,40 +9,34 @@ const path = require('path');
 const logger = require('./utils/logger.utils');
 const adminAccountMiddleware = require('./middlewares/createAdminAccount.middelware');
 
+
 dotenv.config();
 
 const app = express();
 
-
+// body parser
 app.use(express.json());
 
-
+// CORS (تقدر تخصّص origin لو حبيت)
 app.use(cors());
 
-
+// serve uploads folder so images are reachable via:
+// http://HOST:PORT/uploads/<filename>
 const uploadsPath = path.join(__dirname, 'uploads');
 app.use('/uploads', express.static(uploadsPath));
 
-
+// routes
 app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/urls', require('./routes/url.routes'));
 app.use('/api/vuln', require('./routes/vuln.routes'));
+app.use('/api/results', require('./routes/results.routes'));
 app.use('/api/logs', require('./routes/log.routes'));
 app.use('/api/testimonials',require('./routes/testimonials.routes'))
 app.use('/api/achievements',require('./routes/Achievement.routes'))
 app.use('/api/demovideo',require('./routes/demovideo.routes'))
 app.use('/api/download',require('./routes/download_file.routes'))
 
-<<<<<<< Updated upstream
 // basic error handler (so multer/file errors return nice message)  
-=======
-
-app.use('/api/results', require('./routes/results.routes')); 
-
-
-
-
->>>>>>> Stashed changes
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
   if (res.headersSent) return next(err);
@@ -47,25 +44,27 @@ app.use((err, req, res, next) => {
 });
 
 
+// start server AFTER DB connected
 const PORT = process.env.PORT || 3000;
 
-
+    //
+//لازم تبقي في اخر الكود
+///error handing
 const AppError = require('./utils/app.error-utils');
 const golbalErrorHandler = require('./middlewares/error-handelar.middelware');
-
-app.use((req, res, next) => {
+app.use((req,res,next)=>{
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-
+})
 app.use(golbalErrorHandler);
 
 
 (async () => {
   try {
-    await connectDB(); 
+    await connectDB(); // تأكد أن connectDB يعيد promise
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
-      if(logger) logger.info(`Server is running on port ${PORT}`);
+      logger.info(`Server is running on port ${PORT}`);
+    //   console.log(`Serving uploads folder from: ${uploadsPath}`);
     });
     adminAccountMiddleware.createAdminAccount();
   } catch (err) {
